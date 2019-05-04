8a9c8c0a3d9f3b25ad85e53ec3adca954f4a3818
Parvin Faraji <47976214+pfaraji@users.noreply.github.com>
2019-05-04 10:27:21 +0430

    none

---
+++ webproduct-2/webproduct-2/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webproduct_2.Controllers
+{
+    public class ProductController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+           AWEntities ctx = new AWEntities();
+            var products = ctx.Products.ToString();
+
+            return View(products);
+        }
+    }
+}
---
+++ webproducts/webproducts/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webproducts.Controllers
+{
+    public class ProductController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            AdventureWorks2017Entities ctx = new AdventureWorks2017Entities();
+            var products = ctx.Products.ToString();
+            return View(products);
+        }
+    }
+}
